e2fdbe5191dd8be22404988c7b6e09557cc2e2dd
Brian Takita <brian.takita@gmail.com>
2008-05-01 01:44:18 -0700

    Readding /login and /logout.

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ ActionController::Routing::Routes.draw do |map|
   map.lobby '/', :controller => 'lobby', :action => 'show'
   map.activate '/activate/:activation_code', :controller => 'users', :action => 'activate', :activation_code => nil
   map.signup '/signup', :controller => 'users', :action => 'new'
-  map.resources :logins
-#  map.login '/login', :controller => 'sessions', :action => 'new'
-#  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+  map.login '/login', :controller => 'sessions', :action => 'new'
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
 end
